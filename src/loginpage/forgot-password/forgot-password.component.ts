053abc7237ae9c4f6675e0d3569ec6356990a106
Harish <harish.r@blindmatrix.com>
2026-02-04 17:08:55 +0530

    forget password changes

--- src/loginpage/forgot-password/forgot-password.component.ts
+++ src/loginpage/forgot-password/forgot-password.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+  email: any;
+  forgetForm: any;
+  otpSent: boolean = false;
+  constructor(){
+    this.forgetForm = new FormGroup({
+      email: new FormControl('', Validators.email),
+      otp: new FormControl('', Validators.required)
+    })
+  }
 
+  sndOTP(){
+    console.log(this.forgetForm.controls.email.invalide)
+    if(this.forgetForm.controls.email.invalide){
+      this.otpSent = false;
+    } else {
+      this.otpSent = true;
+    }
+  }
+
+  varifyOtp(){
+    this.otpSent = true
+  }
 }
