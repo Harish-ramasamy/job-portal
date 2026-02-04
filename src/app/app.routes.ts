6a600fc1a3a5e52438141371e440c2520e019295
Harish <harish.r@blindmatrix.com>
2026-02-04 16:27:34 +0530

    completed login and move to forget password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ForgotPasswordComponent } from '../loginpage/forgot-password/forgot-password.component';
+import { LoginComponent } from '../loginpage/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: LoginComponent
+    },
+    {
+        path: 'forget-password',
+        component: ForgotPasswordComponent,
+        title: 'Forget Password'
+    }
+];
--- src/loginpage/login/login.component.ts
+++ src/loginpage/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,20 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class LoginComponent {
   bckclr: any = false;
   loginForm: any;
-  constructor() {
+  overAllUser = [
+    {
+      name: 'Hina',
+      passWord: '1234'
+    },
+    {
+      name: 'harish',
+      passWord: '1234'
+    }
+  ];
+  showData: boolean = false;
+  invalide: boolean = false;
+  showPass: boolean = false;
+  constructor(private route: Router) {
     this.loginForm = new FormGroup({
       userName: new FormControl(''),
       passWord: new FormControl('')
@@ -27,11 +41,34 @@ export class LoginComponent {
   }
 
   onSubmit(){
+    if (this.loginForm.get('userName').value) {
+      let userData: any = [];
+      this.overAllUser.forEach((res: any, ind ) => {
+        if (this.loginForm.get('userName').value == res.name && this.loginForm.get('passWord').value === res.passWord) {
+          userData.push(res)
+          this.showData = true;
+
+        } else {
+          this.invalide = true
+        }
+      })
+    } else {
+      this.invalide = true;
+    }
     console.log(this.loginForm.value)
   }
+
+
   onchange(event: any){
     if(this.loginForm.get('userName').value.length > 1){
       this.bckclr = true
     }
+  }
+
+  showPassfn(val: any){
+    this.showPass = val === 'show' ? true : false;
+  }
+  routeFn(){
+    this.route.navigate(['/forget-password'])
   }
 }
